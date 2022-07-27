cfee4a0d828c74970b8adde9e737f65a7186b7b9
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-27 18:44:28 +0530

    insert element in stack 56-30-70

---
+++ Day14StackQueueProblem/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14StackQueueProblem
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+}
